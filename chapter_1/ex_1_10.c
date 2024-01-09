9e7a97a1864bd523d7685a7d02a860883df88e01
Vasilis Anagnostopoulos <vanagnos1@gmail.com>
2024-01-09 20:46:09 +0200

    Add Exercise 1-10

---
+++ chapter_1/ex_1_10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+// just skip printing the second consecutive blank character found 
+int main()
+{
+    int c, lastc;
+    lastc = EOF;
+    
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (lastc != ' ') {
+                putchar(c);
+            }
+        } else if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(c);
+        }
+        lastc = c;
+    }
+
+    return 0;
+}
+
+} 
+
