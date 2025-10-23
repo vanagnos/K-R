fe1e757b176b29f520f025a4e58a28e308f86295
Vasilis Anagnostopoulos <vasilis.anagnostopoulos.ext@ericsson.com>
2025-10-23 18:08:05 +0300

    Add exercises 1_6 and 1_8

--- chapter_1/ex_1_6.c
+++ chapter_1/ex_1_6.c
@@ -1,13 +1,9 @@
  #include <stdio.h>
- /* copy input to output; 1st version */
- int main()
- {
-	 int c;
-	 c = getchar();
-	 while ((c != EOF) == 1) {
-		 putchar(c);
-		 c = getchar();
-	 }
-	 return 0;
+
+ int main(){
+    int c;
+    while ((c = getchar()) != EOF){
+        putchar(c);
+    }
  }
 
--- chapter_1/ex_1_8.c
+++ chapter_1/ex_1_8.c
@@ -1,15 +1,22 @@
  #include <stdio.h>
- /* count lines in input */
- int main()
- {
-	 int c, nl;
-	 nl = 0;
-	 while ((c = getchar()) != EOF){
-		 if (c == '\n' || c == '\t' || c == ' '){
-			 ++nl;
-	         printf("%d\n", nl);
-		 }
-	 }
+ /* count newlines, tabs and blanks in input */
+int main(){
+    int c; 
+    int nl = 0, tb = 0 , bl = 0;
+    while ((c = getchar()) != EOF){
+        if (c == '\n'){
+           ++nl;
+        }
+        else if (c == '\t'){
+           ++tb;
+        }
+        else if (c == ' ' ){
+           ++bl;
+        }
+    } 
+    printf("New lines: %d\n", nl);
+    printf("Tabs: %d\n", tb);
+    printf("Blanks: %d\n", bl);
 
-	 return 0;
+
  } 
