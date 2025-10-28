18f7ee5c4fc55547a79b58f80a6521894965cf2d
Vasilis Anagnostopoulos <vasilis.anagnostopoulos.ext@ericsson.com>
2025-10-28 08:47:37 +0200

    Complete exercise 5-5

--- chapter_5/ex_5_5.c
+++ chapter_5/ex_5_5.c
@@ -3,9 +3,6 @@
 char *strncpy_custom(char *s, const char *t, int n) {
     char *start = s;
 
-    while (*start != '\0')
-        start++;
-
     while (n > 0 && *t != '\0') {
         *start++ = *t++;
         n--;
@@ -17,17 +14,67 @@ char *strncpy_custom(char *s, const char *t, int n) {
         n--;
     }
 
+    return start;
+}
+char *strncat_custom(char *s, const char *t, int n) {
+    char *start = s;
+
+    while (*start != '\0')
+        start++;
+
+    while (n > 0 && *t != '\0') {
+        *start++ = *t++;
+        n--;
+    }
+
+    while (n > 0 && *t == '\0') {
+        *start++ = '\0';
+        n--;
+    }
+
     *start = '\0';
     return start;
+}
+int strncmp_custom(const char *s, const char *t, int n) {
+    while (n > 0 && *s != '\0' && *t != '\0') {
+        if (*s < *t){
+            return (*t - *s);
+        } else if (*s > *t){
+            return (*s - *t);
+        }
+
+        s++;
+        t++;
+        n--;
+    }
+
+
+    return 0;
 }
 
 int main(void) {
     char src[] = "world!";
     char dest[14] = "Hello, ";
 
-    strncpy_custom(dest, src, 6);
+    strncpy_custom(dest, src, 2);
 
-    printf("Copied string: '%s'\n", dest);
+    printf("Copied string: %s\n", dest);
+
+    char src1[] = "world!";
+    char dest1[14] = "Hello, ";
+    strncat_custom(dest1, src1, 6);
+    printf("concatenated string: %s\n", dest1);
+
+    const char *a = "hello";
+    const char *b = "helium";
+    int result;
+
+    result = strncmp_custom(a, b, 3);
+    printf("Result for first 3 chars: %d\n", result);
+
+    result = strncmp_custom(a, b, 5);
+    printf("Result for first 5 chars: %d\n", result);
+
     return 0;
 }
 
